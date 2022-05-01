febd89c3a605a2c44fe7757b0faf2051e38134b5
michaelfoz <101305696+michaelfoz@users.noreply.github.com>
2022-05-01 11:13:38 -0700

    Add files via upload
    Changed "from_date" and "to_date" in "dept_emp" and "titles" from INTs to DATEs; pg admin 4 was not uploading CSV files because INT was supposed to be DATE.

--- schema1.sql
+++ schema1.sql
@@ -43,17 +43,18 @@ CREATE TABLE salaries (
 CREATE TABLE titles (
   emp_no INT NOT NULL,
   title VARCHAR NOT NULL,
-  from_date INT NOT NULL,
-  to_date INT NOT NULL,
+  from_date DATE NOT NULL,
+  to_date DATE NOT NULL,
   FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
   PRIMARY KEY (emp_no, from_date)
 );
 
-CREATE TABLE dept_emp (
-  emp_no INT NOT NULL,
-  dept_no VARCHAR (4) NOT NULL,
-  from_date INT NOT NULL,
-  to_date INT NOT NULL,
-  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-  PRIMARY KEY (emp_no, dept_no)
+CREATE TABLE dept_emp(
+	emp_no INT NOT NULL,
+	dept_no VARCHAR(4) NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+	PRIMARY KEY (emp_no, dept_no)
 );
