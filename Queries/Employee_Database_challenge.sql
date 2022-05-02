b610bc57e35ccacdb544297bd8b7ee30b35081ca
michaelfoz <101305696+michaelfoz@users.noreply.github.com>
2022-05-01 23:44:15 -0700

    Add files via upload

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -1,19 +1,16 @@
 -- *** I saved retirement_titles.csv, unique_titles.csv and retiring_titles.csv into my Data Folder upon completing Deliverable 1.***
+-- *** I saved mentorship_eligibility.csv ito my Data folder upon completing Deliverable 2.***
 
 -- Module 7 Challenge
 -- https://courses.bootcampspot.com/courses/1225/assignments/24838?module_item_id=498094
 
--- Deliverable 1:
+-- Deliverable 1: The Number of Retiring Employees by Title
+-- Using the ERD you created in this module as a reference and your knowledge of SQL queries, create a Retirement Titles table that holds all the titles of employees who were born between January 1, 1952 and December 31, 1955. Because some employees may have multiple titles in the database—for example, due to promotions—you’ll need to use the DISTINCT ON statement to create a table that contains the most recent title of each employee. Then, use the COUNT() function to create a table that has the number of retirement-age employees by most recent job title. Finally, because we want to include only current employees in our analysis, be sure to exclude those employees who have already left the company.
 -- 1. Retrieve the emp_no, first_name, and last_name columns from the Employees table.
 -- 2. Retrieve the title, from_date, and to_date columns from the Titles table.
 -- 3. Create a new table using the INTO clause.
 -- 4. Join both tables on the primary key.
 -- 5. Filter the data on the birth_date column to retrieve the employees who were born between 1952 and 1955. Then, order by the employee number.
--- 6. Export the Retirement Titles table from the previous step as retirement_titles.csv and save it to your Data folder in the Pewlett-Hackard-Analysis folder.
--- 7. Before you export your table, comfirm that it looks like this image:
--- https://courses.bootcampspot.com/courses/1225/files/1402710/preview
-
--- Note: There are duplicate entries for some employees because they have switched titles over the years. Use the following instructions to remove these duplicates and keep only the most recent title of each employee.
 
 SELECT e.emp_no,
 	e.first_name,
@@ -30,6 +27,12 @@ FROM employees AS e
 
 SELECT * FROM retirement_titles;
 
+-- 6. Export the Retirement Titles table from the previous step as retirement_titles.csv and save it to your Data folder in the Pewlett-Hackard-Analysis folder.
+-- 7. Before you export your table, comfirm that it looks like this image:
+-- https://courses.bootcampspot.com/courses/1225/files/1402710/preview
+
+-- Note: There are duplicate entries for some employees because they have switched titles over the years. Use the following instructions to remove these duplicates and keep only the most recent title of each employee.
+
 
 
 -- 8. Copy the query from the Employee_Challenge_starter_code.sql and add it to your Employee_Database_challenge.sql file.
@@ -41,9 +44,6 @@ SELECT * FROM retirement_titles;
 -- 11. Exclude those employees that have already left the company by filtering on to_date to keep only those dates that are equal to '9999-01-01'.
 -- 12. Create a Unique Titles table using the INTO clause.
 -- 13. Sort the Unique Titles table in ascending order by the employee number and descending order by the last date (i.e., to_date) of the most recent title.
--- 14. Export the Unique Titles table as unique_titles.csv and save it to your Data folder in the Pewlett-Hackard-Analysis folder.
--- 15. Before you export your table, confirm that it looks like this image:
--- https://courses.bootcampspot.com/courses/1225/files/1402763/preview
 
 SELECT DISTINCT ON (emp_no) emp_no,
     first_name,
@@ -56,6 +56,10 @@ ORDER BY emp_no ASC, to_date DESC;
 
 SELECT * FROM unique_titles;
 
+-- 14. Export the Unique Titles table as unique_titles.csv and save it to your Data folder in the Pewlett-Hackard-Analysis folder.
+-- 15. Before you export your table, confirm that it looks like this image:
+-- https://courses.bootcampspot.com/courses/1225/files/1402763/preview
+
 
 
 -- 16. Write another query in the Employee_Database_challenge.sql file to retrieve the number of employees by their most recent job title who are about to retire.
@@ -63,9 +67,6 @@ SELECT * FROM unique_titles;
 -- 18. Then, create a Retiring Titles table to hold the required information.
 -- 19. Group the table by title, then sort the count column in descending order.
 -- 20. Export the Retiring Titles table as retiring_titles.csv and save it to your Data folder in the Pewlett-Hackard-Analysis folder.
--- 21. Before you export your table, confirm that it looks like this image:
--- https://courses.bootcampspot.com/courses/1225/files/1403196/preview
--- Save your Employee_Database_challenge.sql file in your Queries folder in the Pewlett-Hackard folder.
 
 SELECT COUNT (ut.emp_no), ut.title
 INTO retiring_titles
@@ -74,3 +75,43 @@ GROUP BY ut.title
 ORDER BY COUNT (ut.emp_no) DESC;
 
 SELECT * FROM retiring_titles;
+
+-- 21. Before you export your table, confirm that it looks like this image:
+-- https://courses.bootcampspot.com/courses/1225/files/1403196/preview
+-- Save your Employee_Database_challenge.sql file in your Queries folder in the Pewlett-Hackard folder.
+
+
+-- Deliverable 2: The Employees Eligible for the Mentorship Program
+-- In the Employee_Database_challenge.sql file, write a query to create a Mentorship Eligibility table that holds the employees who are eligible to participate in a mentorship program.
+-- 1. Retrieve the emp_no, first_name, last_name, and birth_date columns from the Employees table.
+-- 2. Retrieve the from_date and to_date columns from the Department Employee table.
+-- 3. Retrieve the title column from the Titles table.
+-- 4. Use a DISTINCT ON statement to retrieve the first occurrence of the employee number for each set of rows defined by the ON () clause.
+-- 5. Create a new table using the INTO clause.
+-- 6. Join the Employees and the Department Employee tables on the primary key.
+-- 7. Join the Employees and the Titles tables on the primary key.
+-- 8. Filter the data on the to_date column to all the current employees, then filter the data on the birth_date columns to get all the employees whose birth dates are between January 1, 1965 and December 31, 1965.
+-- 9. Order the table by the employee number.
+
+SELECT DISTINCT ON (e.emp_no) e.emp_no,
+    e.first_name,
+    e.last_name,
+    e.birth_date,
+    de.from_date,
+    de.to_date,
+    ti.title
+INTO mentorship_eligibility
+FROM employees AS e
+	INNER JOIN dept_emp AS de
+		ON (e.emp_no = de.emp_no)
+	INNER JOIN titles as ti
+		ON (e.emp_no = ti.emp_no)
+			WHERE (de.to_date = '9999-01-01')
+				AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+				ORDER BY e.emp_no ASC;
+
+SELECT * FROM mentorship_eligibility;
+
+-- 10. Export the Mentorship Eligibility table as mentorship_eligibilty.csv and save it to your Data folder in the Pewlett-Hackard-Analysis folder.
+-- 11. Before you export your table, confirm that it looks like this image:
+-- https://courses.bootcampspot.com/courses/1225/files/1402710/preview
